1bb4c94f4bb2e82f6632cc080df6754f93aa599f
AbdiJavad <Mirjavadabdi@gmail.com>
2023-12-11 22:52:26 +0330

    Updated,Changed

--- src/SliderDemo.java
+++ src/SliderDemo.java
@@ -12,7 +12,6 @@ public class SliderDemo extends Application {
     @Override
     public void start(Stage primaryStage) {
         Text text = new Text(20, 20, "JavaFX Programming");
-
         
         Slider slHorizontal = new Slider();
         slHorizontal.setShowTickLabels(true);
